2baca49e005db21badee07d21ecd1858b2a8bf37
Taylor <wwwaaavvveeerrr@gmail.com>
2019-07-10 15:51:34 -0400

    rebuilt the client

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <>
+    <div className="Title">COMING + SOON</div>
+    </>
+    );
+  }
+}
+
+export default App;
