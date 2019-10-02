a2a5a0b1b449e47cc03a7845c26a4dd71006bbb6
Taylor <wwwaaavvveeerrr@gmail.com>
2019-10-01 22:06:28 -0400

    leaving the desktop version as experimental for now

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.sass';
-import Desktop from './comp/Desktop'
-// import Soon from "./comp/Soon"
+import Soon from "./comp/Soon"
 
 
 class App extends Component {
   render() {
     return (
     <>
-    <Desktop/>
+    <Soon/>
     </>
     );
   }
