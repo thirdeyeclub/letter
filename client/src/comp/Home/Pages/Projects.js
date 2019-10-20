fc219719ae78c293614e6e51697ebbfa66cf7453
Taylor <wwwaaavvveeerrr@gmail.com>
2019-10-20 18:35:24 -0400

    created project cards

--- client/src/comp/Home/Pages/Projects.js
+++ client/src/comp/Home/Pages/Projects.js
@@ -5,17 +5,23 @@ import './projects.sass'
 export default function Projects() {
     return (
         <div className="page">
+
             <div className="title">
                 <h2>Projects</h2>
                 <p>descrption</p>
                 ------------------
             </div>
+
             <div className="content">
+                {/* map function to create each card  */}
             {data.map( alpha => {
             return  <div key={alpha.id} className="pCard">
+
                         <h5 className="title">{alpha.title}</h5>
                         <p>{alpha.descriptor}</p>
-                        <div>{alpha.picture}</div>
+                        <div className="image-cart">
+                        <img alt="of project" key={alpha.picture} src={require(`../../../assets/${alpha.picture}.png`)} className="picture" />
+                        </div>
                         <p>{JSON.stringify(alpha.tags)}</p>
                     </div>
             })}
--- client/src/data/projDat.js
+++ client/src/data/projDat.js
@@ -4,35 +4,35 @@ const data = [
         title : "Live Safe",
         descriptor: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         tags:['#Maps','#Data Visualization','#Javascript'],
-        picture:"[]"
+        picture: "livesafe1"
     },
     {
         id: 2,
         title : "Supp2U",
         descriptor: " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         tags:['#ECommerce','#Ruby'],
-        picture:" []"
+        picture:"livesafe1"
     },
     {
         id: 3,
         title : "VR-Funding Platform",
         descriptor: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
         tags: ['#API','#Node'],
-        picture:"[] "
+        picture:"livesafe1"
     },
     {
         id: 4,
         title : "Slackr",
         descriptor: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
         tags: ['#Slack', '#Javascript' ],
-        picture:" []"
+        picture:"livesafe1"
     },
     {
         id: 5,
         title : "Mentor Me",
         descriptor: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
         tags: ['#Javascript'],
-        picture:" []"
+        picture:"livesafe1"
     }
 ]
 
