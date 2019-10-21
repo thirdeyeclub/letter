ba5f46dc8680990632e0f6bb102c4540dbc80eb7
Taylor <wwwaaavvveeerrr@gmail.com>
2019-10-21 17:15:49 -0400

    connected to a server with axios

--- client/src/comp/Home/Pages/Contact.js
+++ client/src/comp/Home/Pages/Contact.js
@@ -1,22 +1,31 @@
 import React, {useState} from 'react'
 
+import axios from 'axios'
+
 import './contact.sass'
 
 export default function Contact() {
 
-    const [Email, setEmail] = useState({})
+    const [Email, setEmail] = useState({
+        date: new Date(),
+        sender: null,
+        title: null,
+        body: null})
 
     const changeHandler = event => {
         setEmail({ ...Email, [event.target.name]: event.target.value });
     };
     const sendMail = event =>{
         event.preventDefault()
-        console.log('CLICKED', Email)  
+        console.log('CLICKED', Email) 
+
+        axios.post(`https://countervail-33.herokuapp.com/eDrop`, Email)
+        .then(res => console.log(res.data))
+        .catch(error => console.log('THERE HAS BEEN AN ERROR',error))
         // this function will send an object to my backend 
         // then send that to my email blount3taylor@gmail.com
 
-    } 
-    
+    }  
 
     return (
         <div className="contact-page">
@@ -28,7 +37,7 @@ export default function Contact() {
                     className="input" 
                     type="text" 
                     placeholder="Your email"
-                    name="email"
+                    name="sender"
                     onChange={changeHandler}/><br/>
 
                 <label>What is this regarding</label><br/>
