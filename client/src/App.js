b22227a8c3ac8317c0ece04facc01fcc94459471
Taylor <wwwaaavvveeerrr@gmail.com>
2019-10-28 03:18:36 -0400

    created new coming soon page

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.sass';
 
 import Home from './comp/Home/Home'
-
+import Soon from './comp/Soon'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-    <Home />
+    <Soon />
       </div>
     );
   }
